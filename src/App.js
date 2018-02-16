19a116a260371b14b347d10800575d09579b1c62
Bhaumik Dedhia <bhaumikbcg@yahoo.com>
2018-02-16 14:09:38 -0800

    Added the Bootstrap css and js files. Created form with dropdown. Removed the React Image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 //React files
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+//import './css/bootstrap.min.css';
+//import './js/bootstrap';
 import test from './bw_src/BW_TableManagerTest.js'
 
 class App extends Component {
   render() {
     return (
+      <div class="container">
       <div className="App">
         <header className="BW_Header">
-          <img src={logo} alt="bw"/>
           <h1 className="BW_Title">Welcome to Boardwalk</h1>
-          <h2>{test('testTableManager')}</h2>
+          <form>
+            <div class="form-group">
+            <label for="bw_test">Boardwalk Test</label>
+            <select class="form-control form-control-sm" id="bw_test">
+              <option>testTableManager</option>
+              <option>testcolumns</option>
+            </select>
+            </div>
+          </form>
+          <h2>{JSON.stringify(test(document.getElementById('bw_test')))}</h2>
         </header>
+      </div>
       </div>
    );
   }
--- src/bw_src/BW_TableManagerTest.js
+++ src/bw_src/BW_TableManagerTest.js
@@ -1,20 +1,22 @@
 import BW_TableManager from './BW_TableManager'
 export default function test(s) {
+    var a = {};
     
     switch(s){
         case 'testTableManager':
             var tm = new BW_TableManager()
             tm.add (123,"samyak","notsure",0,0,0,0,123,0,0,0,0,0)
-            tm.get(123)
+            a = tm.get(123)
             break;
         case 'testcolumns':
             var tmc = new BW_TableManager();
             tmc.add (123,"samyak","notsure",0,0,0,0,123,0,0,0,0,0)
             var t1 = tmc.get(123)
             t1.addcolumn(456, "samyak1",t1.id,0,0)
-            return t1.getcolumn(456)
-            //break;
+            a = t1.getcolumn(456)
+            break;
         default:
             console.log("default case");
     }
+    return a;
 }
