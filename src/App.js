3895050177c3290d7401845c7a83e2dff0fcaab3
bhaumikbcg <bhaumikbcg@yahoo.com>
2018-02-21 15:39:33 -0800

    Created a new component for Dropdown and moved the code to that file. Imported the dropdown component in App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 //React files
 import React, { Component } from 'react';
-import test from './bw_src/BW_TableManagerTest.js'
+import Dropdown from './form_components/Dropdown'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {value: 'test'};
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event){
-    this.setState({value: event.target.value});
-  }
-  getValue(){
-    return this.selectValue.value;
-  }
   render() {
     return (
       <div className="container">
-      <div className="App">
-        <header className="BW_Header">
-          <h1 className="BW_Title">Welcome to Boardwalk</h1>
-          <form>
-            <div className="form-group">
-            <label for="bw_test">Boardwalk Test</label>
-            <select value={this.state.value} onChange={this.handleChange} className="form-control form-control-sm"  id="bw_test" >
-              <option value="test">Test</option>
-              <option value="testTableManager">testTableManager</option>
-              <option value="testcolumns">testcolumns</option>
-              <option value="testrows">testrows</option>
-              <option value="testcells">testcells</option>
-              <option value="testCellByRow">testCellByRow</option>
-              <option value="testCellByColumn">testCellByColumn</option>
-            </select>
-            </div>
-          </form>
-          <h2>{JSON.stringify(test(this.state.value))}</h2>
-        </header>
-      </div>
+        <div className="App">
+          <header className="BW_Header">
+            <h1 className="BW_Title">Welcome to Boardwalk</h1>
+          </header>
+          <div className="row">
+            <Dropdown />
+          </div>
+        </div>
       </div>
    );
   }
---
+++ src/form_components/Dropdown.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import test from '../bw_src/BW_TableManagerTest'
+export default class Dropdown extends Component{
+    constructor(){
+        super()
+        this.state = {value: 'test'};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+    getValue(){
+        return this.selectValue.value;
+    }
+
+    render(){
+        return(
+            <div className="col-sm">
+            <form>
+                <div className="form-group">
+                    <label for="bw_test">Boardwalk Test</label>
+                    <select value={this.state.value} onChange={this.handleChange} className="form-control form-control-sm"  id="bw_test" >
+                        <option value="test">Test</option>
+                        <option value="testTableManager">testTableManager</option>
+                        <option value="testcolumns">testcolumns</option>
+                        <option value="testrows">testrows</option>
+                        <option value="testcells">testcells</option>
+                        <option value="testCellByRow">testCellByRow</option>
+                        <option value="testCellByColumn">testCellByColumn</option>
+                    </select>
+                </div>
+            </form>
+            <h2>{JSON.stringify(test(this.state.value))}</h2>
+            </div>
+        );
+    }
+}
